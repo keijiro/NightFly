2c385841301881eeb3233e7a9326157b7b1b7cdc
Keijiro Takahashi <keijiro@gmail.com>
2018-01-18 20:55:36 +0900

    Tweaks

---
+++ Assets/NightFly/BlitToBG.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BlitToBG : MonoBehaviour
+{
+    [SerializeField] Camera _targetCamera;
+
+    CommandBuffer _cmd;
+    RenderTexture _tempRT;
+
+    void Start()
+    {
+        _tempRT = new RenderTexture(1920, 1080, 24);
+
+        GetComponent<Camera>().targetTexture = _tempRT;
+
+        _cmd = new CommandBuffer();
+        _cmd.Blit(_tempRT, BuiltinRenderTextureType.CameraTarget);
+
+        _targetCamera.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, _cmd);
+    }
+
+    void OnDestroy()
+    {
+        Destroy(_tempRT);
+    }
+}
